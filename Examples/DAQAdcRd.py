52375d06c0e90581381c8727c9a6ba72d15344a0
nhayes111 <nhayes111@gmail.com>
2009-11-04 13:46:19 +0000

    daqX.py and daqxh.py are working, and non-compiled version to allow user editing.  Examples were added and updated to reflect any addition changes.

--- Examples/DAQAdcRd.py
+++ Examples/DAQAdcRd.py
@@ -2,10 +2,10 @@ import daqX
 
 dev = daqX.daqDevice('DaqBoard2K0')
 chan = 0
-gain = 1
-flags = ['DafUnipolar','DafUnsigned']
+gain = 'DgainX1'
+flags = ['DafBipolar','DafUnsigned']
 
 read = dev.AdcRd(chan, gain, flags)
-print read, 'Volts'
-
+read = (20.0/2**16)*read -10
+print read
 dev.Close()
